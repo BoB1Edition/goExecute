020d74b7231b939c9fbacdeda2f5b51515946738
Жохов Александр <a.zhohov@crpt.ru>
2019-07-17 11:53:04 +0300

    rjvvbn

--- test.go
+++ test.go
@@ -6,11 +6,12 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+
+	"github.com/gotk3/gotk3/gtk"
 )
 
 type word [2]byte
-
-//type
+type dword [4]byte
 
 type MZHeader struct {
 	Magic             word
@@ -27,20 +28,32 @@ type MZHeader struct {
 	InitialCS         word
 	RelocationTable   word
 	Overlay           word
-	e_res1            [4]word
+	E_res1            [4]word
 	Oemid             word
 	Oeminfo           word
-	e_res2            [10]word
+	E_res2            [10]word
+	E_lfanew          dword
 }
 
 type PEHeader struct {
-	Data []byte
+	//Data []byte
 }
 
 type EXEstruct struct {
 	*MZHeader
 	*PEHeader
-	Data []byte
+	//Data []byte
+}
+
+func (word *word) toUint16() uint16 {
+	//t := []byte{word[0], word[1]}
+	ret := binary.LittleEndian.Uint16([]byte{word[0], word[1]})
+	return ret
+}
+
+func (dword *dword) toUint32() uint32 {
+	ret := binary.LittleEndian.Uint32([]byte{dword[0], dword[1], dword[2], dword[3]})
+	return ret
 }
 
 func (mzheader *MZHeader) isPE() bool {
@@ -52,6 +65,9 @@ func (mzheader *MZHeader) isPE() bool {
 }
 
 func main() {
+	gtk.Init(nil)
+	//gtk.
+	//win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
 	pathEXE := flag.String("pathEXE", "c:\\windows\\notepad.exe", "path to disasamble execute")
 	flag.Parse()
 	data, err := ioutil.ReadFile(*pathEXE)
@@ -66,10 +82,11 @@ func main() {
 	execute.PEHeader = &peheader
 	binary.Read(bytes.NewReader(data), binary.LittleEndian, &mzheader)
 	fmt.Println("Magic: ", execute.Magic)
-	fmt.Println("IP: ", execute.InitialIP)
+	fmt.Println("IP: ", execute.InitialIP.toUint16())
 	fmt.Println("CS: ", execute.InitialCS)
 	fmt.Println("SP: ", execute.InitialSP)
 	fmt.Println("SS: ", execute.InitialSS)
+	fmt.Println("RelocationTable: ", execute.RelocationTable.toUint16())
+	fmt.Println("E_lfanew: ", execute.E_lfanew.toUint32())
 	fmt.Println("mzheader: ", mzheader)
-
 }
